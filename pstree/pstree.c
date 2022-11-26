ea71197143cbcf4750e3de2df56cc33f6a5ac290
tracer-nju <tracer@nju.edu.cn>
2022-11-26 18:33:11 +0800

    Linux DESKTOP-K3QFC4A 4.4.0-19041-Microsoft #1237-Microsoft Sat Sep 11 14:32:00 PST 2021 x86_64 x86_64 x86_64 GNU/Linux  18:33:11 up 2 days,  5:31,  0 users,  load average: 0.52, 0.58, 0.59

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -49,7 +49,10 @@ int main(int argc, char *argv[]) {
             sprintf (fullpath, "%s/%s/stat", PATH, entry->d_name);
             fd = open (fullpath, O_RDONLY);
             read (fd, buf, READ_LEN);
-            sscanf (buf, "%d (%s) %c %d", &pid, name, &state, &ppid);
+
+            printf ("%s\n", buf);
+
+            sscanf (buf, "%d (%s) %c %d ", &pid, name, &state, &ppid);
             procs[proc_count].pid = pid;
             procs[proc_count].ppid = ppid;
             name[strlen(name)-1] = 0;
