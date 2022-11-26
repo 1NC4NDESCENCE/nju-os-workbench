83a5cd48696694ec53ac234a72f096b0b7e21197
tracer-nju <tracer@nju.edu.cn>
2022-11-26 16:48:35 +0800

    Linux DESKTOP-K3QFC4A 4.4.0-19041-Microsoft #1237-Microsoft Sat Sep 11 14:32:00 PST 2021 x86_64 x86_64 x86_64 GNU/Linux  16:48:35 up 2 days,  3:47,  0 users,  load average: 0.52, 0.58, 0.59

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -4,6 +4,11 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <dirent.h>
+#include <stdbool.h>
+
+const char PATH[] = "~/proc";
+const int READ_LEN = 256;
 
 typedef struct _proc _proc;
 
@@ -14,12 +19,56 @@ struct _proc {
     _proc** children;
 };
 
+bool isNumeric (char* str);
 
 int main(int argc, char *argv[]) {
-  for (int i = 0; i < argc; i++) {
-    assert(argv[i]);
-    printf("argv[%d] = %s\n", i, argv[i]);
-  }
-  assert(!argv[argc]);
+    DIR* dir;
+    struct dirent *entry;
+    dir = opendir (PATH);
+
+    int fd;
+    char fullpath[64];
+    char buf[128];
+    
+    int pid;
+    int ppid;
+    char state;
+    char name[64];
+
+    int proc_capacity = 64;
+    int proc_count = 0;
+    _proc* procs = malloc (sizeof(_proc) * proc_capacity);
+
+    /* get information about processes and store it in a list */
+    while ((entry = readdir (dir)) != NULL)
+    {
+        if (isNumeric (entry->d_name))
+        {
+            sprintf (fullpath, "%s/%s/stat", PATH, entry->d_name);
+            fd = open (fullpath, O_RDONLY);
+            read (fd, buf, READ_LEN);
+            sscanf (buf, "%d (%s) %c %d", pid, name, state, ppid);
+            procs[proc_count].pid = pid;
+            procs[proc_count].ppid = ppid;
+            strcpy (procs[proc_count].name, name);
+            procs[proc_count].children = NULL;
+            if (++proc_count >= proc_capacity) realloc (procs, sizeof(_proc) * (proc_capacity*=2));
+        }
+    }
+
+    for (size_t i=0; i<proc_count; i++)
+    {
+        printf ("process name:\t%s\tpid:\t%d\tppid:\t%d", procs[i].name, procs[i].pid, procs[i].ppid);
+    }
+
+    /* find all the children for any given process */
   return 0;
+}
+
+bool isNumeric (char* str)
+{
+    char* ch = str;
+    do {
+        if (*ch < '0' || *ch > '9') return false;
+    } while (*++ch != '\0');
 }
